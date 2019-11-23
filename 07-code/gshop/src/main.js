66b7a21e85fcc12b9ee3d94e06c7a08046f32ced
24kcs <younghongbo@126.com>
2019-11-23 16:52:29 +0800

    gshop-code

---
+++ 07-code/gshop/src/main.js
@@ -0,0 +1,14 @@
+
+import Vue from 'vue'
+import App from './App'
+// 引入路由器并注册
+import router from './router'
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  router
+})
